20a4b950689f4f70c0286d0bc0123399de3db778
deepabhra <guinabhradeep0304@gmail.com>
2026-08-26 14:21:37 +0530

    complete intersection operation for fuzzy sets

---
+++ semester I/Soft_computing/Lab/Assignment_set_1/intersection.py
@@ -0,0 +1,55 @@
+"""
+Name       : Abhradeep Guin
+Class      : M.Sc. 1st Year
+Assignment 1: Fuzzy Sets and Relations
+Question   : Intersection of two Fuzzy Sets
+"""
+
+def fuzzy_intersection(setA, setB):
+    intersection_set = {}
+    all_elements = set(setA.keys()) | set(setB.keys())
+    for element in all_elements:
+        a_val = setA.get(element, 0)
+        b_val = setB.get(element, 0)
+        intersection_set[element] = min(a_val, b_val)
+    return intersection_set
+
+
+# To avoid invalid input, it keeps asking until the user enters a value between 0 and 1 (inclusive).
+
+def get_valid_membership(valid_input):
+    while True:
+        try:
+            value = float(input(valid_input))
+        except ValueError:
+            print("\nInvalid input! Please enter a number.\n")
+            continue
+        if 0 <= value <= 1:
+            return value
+        else:
+            print("\nInvalid! Membership value must be between 0 and 1. Try again.\n")
+
+
+def input_fuzzy_set(set_name, n):
+    fuzzy_set = {}
+    print(f"\nEnter Values of Fuzzy set {set_name}: ")
+    for i in range(n):
+        element = input(f"Enter element {i+1}: ")
+        membership = get_valid_membership(
+            f"Enter the membership value for {element} in Set {set_name}: "
+        )
+        fuzzy_set[element] = membership
+    return fuzzy_set
+
+
+# ---- Main Program ----
+n = int(input("Enter number of elements: "))
+
+setA = input_fuzzy_set("A", n)
+setB = input_fuzzy_set("B", n)
+
+intersection = fuzzy_intersection(setA, setB)
+
+print("\nFuzzy setA: ", setA)
+print("Fuzzy setB: ", setB)
+print("Intersection (A ∩ B): ", intersection)
